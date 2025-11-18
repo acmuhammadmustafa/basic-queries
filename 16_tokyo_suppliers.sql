360cbf67fae5d69204d73e8319563529b3681258
origin <acmuhammadmustafa@gmail.com>
2025-11-18 14:38:03 -0500

    updated question 16 answer

--- 16_tokyo_suppliers.sql
+++ 16_tokyo_suppliers.sql
@@ -1,4 +1,4 @@
 use northwind;
 select ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
 from products
-where SupplierID = 18;
+where SupplierID = 4;
